dd53cae3a1c8768fc97e60214d49e4b4d42e8bc4
Onuralp <onuralphachamzaoglu@gmail.com>
2023-10-09 21:13:39 +0300

    reminder added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Contact from "@/components/sections/Contact";
 import Info from "@/components/sections/Info";
 import Journey from "@/components/sections/Journey";
+//?: should do SEO
 export default function Home() {
 	return (
 		<main className="main-container  overflow-x-hidden overflow-y-scroll h-screen mx-auto max-w-[1920px] flex flex-col  gap-36">
