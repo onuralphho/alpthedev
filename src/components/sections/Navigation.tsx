360bb8e043191069159f961fdc5ca939ebc5e5b3
Onuralp <onuralphachamzaoglu@gmail.com>
2023-10-16 01:33:19 +0300

    navbar fix

--- src/components/sections/Navigation.tsx
+++ src/components/sections/Navigation.tsx
@@ -6,8 +6,8 @@ type Props = {};
 
 function Navigation({}: Props) {
 	return (
-		<nav className="fixed bottom-0 w-full flex justify-center z-40">
-			<ul className="flex gap-0.5 text-[white] font-bold rounded-t-xl h-40 translate-y-32 lg:translate-y-28 ">
+		<nav className="fixed bottom-0 h-min w-full flex justify-center z-40">
+			<ul className="flex gap-0.5 text-[white] font-bold rounded-t-xl ">
 				<li
                     className="cursor-pointer bg-[#a431eb] p-2 lg:p-4  peer-hover:opacity-60 rounded-tl-xl hover:-translate-y-6 transition-transform hover:bg-[#d187ff]" 
 					onClick={() => {
