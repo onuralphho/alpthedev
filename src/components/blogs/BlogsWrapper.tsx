5bc069351c7c5ae59694744127ed434ac9954010
Onuralp <onuralphachamzaoglu@gmail.com>
2023-11-13 18:46:14 +0300

    --fix

--- src/components/blogs/BlogsWrapper.tsx
+++ src/components/blogs/BlogsWrapper.tsx
@@ -2,21 +2,19 @@
 import React from "react";
 import { type Blog } from "@prisma/client";
 import BlogCard from "./BlogCard";
-import { useRouter } from "next/navigation";
 type Props = {
 	blogs: Blog[];
 };
 
 export default function BlogsWrapper({ blogs }: Props) {
-	const router = useRouter();
-	
-	
 
 	return (
 		<div className="col-span-full sm:col-span-8 md:col-span-9 lg:col-span-3 grid grid-cols-1 p-2 lg:grid-cols-2 overflow-auto max-h-screen  w-full gap-5">
-			{blogs.sort((a,b)=> b.createdAt.getTime() - a.createdAt.getTime()).map((blog) => (
-				<BlogCard key={blog.id} blog={blog} />
-			))}
+			{blogs
+				.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+				.map((blog) => (
+					<BlogCard key={blog.id} blog={blog} />
+				))}
 		</div>
 	);
 }
