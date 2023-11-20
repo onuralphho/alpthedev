8f28e6d9d1bd0dfdb46acf177c120a1c3d12b6ec
Onuralp <onuralphachamzaoglu@gmail.com>
2023-11-20 11:36:44 +0300

    blog date fix

--- src/lib/dateformatter.ts
+++ src/lib/dateformatter.ts
@@ -1,11 +1,16 @@
-"use client"
+const months = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ];
+
 export const dateformatter = (date: Date) => {
 	const currentYear = new Date().getUTCFullYear();
-
+	console.log(date);
 	const day = date.getUTCDate();
-	const month = date.toLocaleDateString("default", { month: "short" });
+	const monthIndex = date.getUTCMonth();
 	const year = date.getUTCFullYear();
 
+	const month = months[monthIndex];
 
 	if (year !== currentYear) {
 		return day + " " + month + " " + year;
