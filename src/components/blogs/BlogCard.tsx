b22daec726a13489a1ee79f738515f6d88a5fd4c
Onuralp <onuralphachamzaoglu@gmail.com>
2023-12-16 12:40:06 +0300

    sitemap fix

--- src/components/blogs/BlogCard.tsx
+++ src/components/blogs/BlogCard.tsx
@@ -4,7 +4,7 @@ import type { Blog } from "@prisma/client";
 import Link from "next/link";
 import { BlogsWithCategoryAndUser } from "@/@types/entityTypes";
 import { dateformatter } from "@/lib/dateformatter";
-
+import profilePicture from "@/assets/profile.jpg";
 type Props = {
 	blog: BlogsWithCategoryAndUser;
 	index: number;
@@ -17,8 +17,8 @@ function BlogCard({ blog, index }: Props) {
 				<div className="flex items-center gap-2">
 					<img
 						className="w-9 aspect-square rounded-full"
-						src="https://budget-app-ivory-two.vercel.app/_next/image?url=http%3A%2F%2Fres.cloudinary.com%2Fdjmonktf8%2Fimage%2Fupload%2Fv1700429176%2Fbudget-images%2Fhkqp3mdcwhq8ibrfm186.webp&w=384&q=75"
-						alt=""
+						src={profilePicture.src}
+						alt="Profile picture"
 					/>
 					<span>{blog.user.displayName} |</span>
 					<span>{dateformatter(blog.createdAt)}</span>
@@ -29,7 +29,7 @@ function BlogCard({ blog, index }: Props) {
 							<h2 className="text-2xl font-bold">{blog.title}</h2>
 							<p className="text-base">{blog.description}</p>
 						</div>
-						<div className="flex justify-around text-xs self-end mt-auto"></div>
+						
 					</div>
 					<div className="h-36 flex flex-col justify-center">
 						<img
